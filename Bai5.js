39b4b302bb77ce94ca2437c537979b5d6bf14ed3
Ninh Phuong <131765757+ninhvietphuongg@users.noreply.github.com>
2023-04-25 22:46:34 +0700

    Delete Bai5.js

--- Bai5.js
+++
@@ -1,6 +0,0 @@
-// Tính chuvi và diện tích của hình tròn khi có bán kính r
-var bankinh = prompt('Nhap ban kinh r:')
-var chuvi = 2 * 3.14 * bankinh;
-var dientich = 3.14 * bankinh * bankinh;
-console.log(chuvi);
-console.log(dientich);
