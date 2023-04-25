6b55b5b2f78686e4e9438d16bc419e89fe578693
Ninh Phuong <131765757+ninhvietphuongg@users.noreply.github.com>
2023-04-25 22:46:27 +0700

    Delete Bai4.js

--- Bai4.js
+++
@@ -1,7 +0,0 @@
-// Tính diện tích của hình thang có đáy trên và đáy dưới
-var daytren = prompt("Nhap day tren:");
-var dayduoi = prompt('Nhap day duoi');
-var tong = Number(daytren) + Number(dayduoi);
-var chieucao = prompt('Nhap day duoi:');
-var dientich = chieucao * (tong / 2);
-console.log(dientich);
