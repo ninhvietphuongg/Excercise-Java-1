90a6e5302178592d13380334caa2555269dc22c3
Ninh Phuong <131765757+ninhvietphuongg@users.noreply.github.com>
2023-04-25 22:46:39 +0700

    Delete Bai6.js

--- Bai6.js
+++
@@ -1,9 +0,0 @@
-//Viết chương trình nhập 2 số a, b. Tính tích, thương.
-var a = prompt('Nhap a:')
-var b = prompt('Nhap b:')
-var tich = a * b;
-var thuong = a / b;
-var layphandu = a % b;
-console.log(tich);
-console.log(thuong);
-console.log(layphandu);
