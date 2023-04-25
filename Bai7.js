16168f1f9595d78a28c99b8efd78d3efa2e67c4c
Ninh Phuong <131765757+ninhvietphuongg@users.noreply.github.com>
2023-04-25 22:46:43 +0700

    Delete Bai7.js

--- Bai7.js
+++
@@ -1,5 +0,0 @@
-//Viết chương trình nhập vào 1 số rồi kiểm tra xem đó là số chẵn hay số lẻ.
-var a = prompt('Nhap vao 1 so bat ki:');
-if(a % 2 == 0){
-    console.log('So chan');
-}   else console.log('So le');
