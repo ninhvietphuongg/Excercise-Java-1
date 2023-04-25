2e18dc021180bb286217896a755c683a038a2c02
ninhvietphuongg <131765757+ninhvietphuongg@users.noreply.github.com>
2023-04-25 17:27:28 +0700

    Add files via upload

---
+++ Bai5.js
@@ -0,0 +1,6 @@
+// Tính chuvi và diện tích của hình tròn khi có bán kính r
+var bankinh = prompt('Nhap ban kinh r:')
+var chuvi = 2 * 3.14 * bankinh;
+var dientich = 3.14 * bankinh * bankinh;
+console.log(chuvi);
+console.log(dientich);
---
+++ Bai6.js
@@ -0,0 +1,9 @@
+//Viết chương trình nhập 2 số a, b. Tính tích, thương.
+var a = prompt('Nhap a:')
+var b = prompt('Nhap b:')
+var tich = a * b;
+var thuong = a / b;
+var layphandu = a % b;
+console.log(tich);
+console.log(thuong);
+console.log(layphandu);
