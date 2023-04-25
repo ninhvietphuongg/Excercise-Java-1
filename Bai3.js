1c1e5d9588dafeb15ed0e369627d378136726b02
Ninh Phuong <131765757+ninhvietphuongg@users.noreply.github.com>
2023-04-25 22:46:23 +0700

    Delete Bai3.js

--- Bai3.js
+++
@@ -1,6 +0,0 @@
-//Tính chu vi và diện tích của hình vuông
-var chieudai = prompt("Nhap chu vi:");
-var chuvi = chieudai * 4;
-var dientich = chieudai*chieudai;
-console.log(chuvi);
-console.log(dientich);
