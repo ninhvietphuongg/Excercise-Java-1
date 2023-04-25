bd17a70b6013df54184ac7bed4c583709a056e72
Ninh Phuong <131765757+ninhvietphuongg@users.noreply.github.com>
2023-04-25 22:46:19 +0700

    Delete Bai2.js

--- Bai2.js
+++
@@ -1,8 +0,0 @@
-// Tính chu vi và diện tích của hình chữ nhật:
-var chieudai = prompt('Nhap chieu dai:');
-var chieurong = prompt('Nhap chieu rong:');
-var tong = Number(chieudai) + Number(chieurong);
-var chuvi = tong * 2;
-var dientich = (chieudai * chieurong);
-console.log(chuvi);
-console.log(dientich);
